d2b0c6ba44e304d182c7017175f889a9cd11c262
Abdullah Malik <7953568+iAmTheAbdullah@users.noreply.github.com>
2020-05-10 16:41:28 +0100

    Implement roll die

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,8 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var diceOne: UIImageView!
+    @IBOutlet weak var diceTwo: UIImageView!
+    
+    let die = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func rollDice(_ sender: Any) {
+        diceOne.image = die.randomElement()
+        diceTwo.image = die.randomElement()
     }
 }
